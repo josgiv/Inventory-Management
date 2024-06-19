a6885a4c531191f733a18bf173eb990f494f6cba
Josia Given Santoso <josgiv1542@gmail.com>
2024-06-19 13:07:52 +0700

    adding route

--- authenticator/app.py
+++ authenticator/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, request, session, send_from_directory
 from flask_session import Session
 import subprocess
-import webbrowser
 import time
 import os
 import sqlite3
+import streamlit
 
 from sympy import true
 
@@ -16,28 +16,22 @@ Session(app)
 
 streamlit_process = None
 
-# Set the static folder to the directory containing your static files
 app.static_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'assets'))
-
-# Path to database file
 DATABASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'database', 'inventaris.db'))
 
 def validate_credentials(username, password):
-    # Connect to the database
+    # Menghubungkan ke database SQLite
     with sqlite3.connect(DATABASE_PATH) as conn:
         cursor = conn.cursor()
-        # Execute SQL query to retrieve user data
         cursor.execute("SELECT * FROM Users WHERE username = ? AND password = ?", (username, password))
-        # Fetch the result
         user = cursor.fetchone()
-        # If user exists and password matches, return True
         if user:
             return True
     return False
 
 @app.route('/')
 def index():
-    # Check if user is logged in, if not, redirect to login page
+    # Cek apakah user telah masuk sebelumnnya. Jika belum, arahkan ke halaman login
     if not session.get("logged_in"):
         session["logged_in"] = False
         return redirect(url_for('login'))
@@ -61,11 +55,8 @@ def welcome():
     global streamlit_process
     if session.get("logged_in"):
         if streamlit_process is None or streamlit_process.poll() is not None:
-            # Run subprocess for Streamlit
             streamlit_process = subprocess.Popen(["streamlit", "run", "../app/Beranda_🏠_.py"], shell=True)
-            # Allow time for Streamlit to start
             time.sleep(2)
-            # Open Streamlit URL in browser
             return render_template('welcome.html')
     return redirect(url_for('login', invalid="true"))
 
@@ -86,7 +77,7 @@ def logout():
     session['logged_in'] = False
     return redirect(url_for('login'))
 
-# Add route to serve images from the 'assets' folder
+# Rute untuk path assets
 @app.route('/assets/<path:filename>')
 def assets(filename):
     return send_from_directory(app.static_folder, filename)
